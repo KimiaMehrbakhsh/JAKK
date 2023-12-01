ce608a81f3e5daf4b6f44f74abb75ba568779ae5
JayDalool <123786050+JayDalool@users.noreply.github.com>
2023-12-01 11:17:57 -0600

    Djtool push

---
+++ src/components/DJTool.tsx
@@ -0,0 +1,39 @@
+//DjTOOL 
+import React from 'react';
+import { ScrollArea, ScrollBar } from '~/components/ui/scroll-area';
+import { Separator } from '~/components/ui/separator';
+import { AlbumArtwork } from '~/components/Artwork';
+import { fetchAlbums, fetchPlaylists } from "~/lib/data";
+
+const DJTool: React.FC = async () => {
+  const Albums = await fetchAlbums();
+  const Playlists = await fetchPlaylists();
+  return (
+    <main className="h-full px-4 py-6 lg:px-8">
+      <div className="space-y-1">
+        <h2 className="text-2xl font-semibold tracking-tight">Your Own DJ Tool</h2>
+        <p className="text-muted-foreground text-sm">All Your DJ essentials in One Place.</p>
+      </div>
+      <Separator className="my-4" />
+      <div className="relative">
+        <ScrollArea>
+          <div className="flex gap-2 space-x-4 pb-4">
+            {Albums.map((album) => (
+              <AlbumArtwork
+                playlists={Playlists}
+                key={album.name}
+                album={album}
+                className="w-[150px]"
+                width={150}
+                height={150}
+              />
+            ))}
+          </div>
+          <ScrollBar orientation="horizontal" />
+        </ScrollArea>
+        </div>
+    </main>
+  );
+};
+
+export default DJTool;
