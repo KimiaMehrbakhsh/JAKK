1a60564e86382dd4fc31ca29893cd8d5992de9cf
kimiamehr <115492092+kimiamehr@users.noreply.github.com>
2023-11-30 22:33:45 -0600

    RecordPlayer and Sliders

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,18 @@
 import Home from "@/components/Home";
+import RecordDisk from "~/components/RecordPlayer";
+
 
 export default function dashboard() {
   return (
     <main className="grid h-screen w-screen grid-cols-[30%,1fr] gap-4 p-8">
-      <section className="h-full rounded-3xl border border-slate-200 bg-white text-slate-950 shadow dark:border-slate-800 dark:bg-slate-950 dark:text-slate-50"></section>
+
+      <section className="h-full rounded-3xl border border-slate-200 bg-white text-slate-950 shadow dark:border-slate-800 dark:bg-slate-950 dark:text-slate-50">
+        <RecordDisk size = "70%" color = "#333"/>
+        
+      </section>
       <section className="h-full overflow-x-auto rounded-3xl border border-slate-200 bg-white text-slate-950 shadow dark:border-slate-800 dark:bg-slate-950 dark:text-slate-50">
         <Home />
+        
       </section>
     </main>
   );
